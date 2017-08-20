437a9181a9e5a5eb7bf2def478b1ec7670dcba74
Stephen Whitmore <sww@eight45.net>
2017-08-19 17:17:18 -0700

    smaller spread

--- ex.js
+++ ex.js
@@ -4,11 +4,12 @@ var memdb = require('memdb')
 var store = GeoStore(memdb(), { tileSize: 50 })
 
 var pending = 10
+var spread = 0.03
 console.time('insert')
 function insert () {
   if (!pending) return check()
-  var x = Math.random() * 200 - 100
-  var y = Math.random() * 200 - 100
+  var x = Math.random() * spread - spread/2
+  var y = Math.random() * spread - spread/2
   var loc = 'hey'
   store.insert([x,y], loc, function (err) {
     pending--
