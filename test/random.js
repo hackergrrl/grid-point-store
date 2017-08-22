2bd25b982b9bc26c823a9b2568a2e6cf4d4753d6
Stephen Whitmore <sww@eight45.net>
2017-08-22 14:23:12 -0700

    add test

---
+++ test/random.js
@@ -0,0 +1,44 @@
+var GeoStore = require('..')
+var test = require('tape')
+var memdb = require('memdb')
+
+test('random points', function (t) {
+  var store = GeoStore(memdb(), { zoomLevel: 8 })
+
+  var bbox = [ [ -5, -5 ], [ 5, 5 ] ]
+  var expected = {}
+
+  var pending = 10000
+  var spread = 20
+  function insert () {
+    if (!pending) return check()
+    var x = Math.random() * spread - spread/2
+    var y = Math.random() * spread - spread/2
+    var loc = Math.random().toString().substring(2)
+    store.insert([x,y], loc, function (err) {
+      pending--
+
+      if (y >= bbox[0][0] && y <= bbox[1][0] &&
+          x >= bbox[0][1] && x <= bbox[1][1]) {
+        expected[loc] = true
+      }
+
+      insert()
+    })
+  }
+  insert()
+
+  function check () {
+    var q = store.queryStream(bbox)
+    var numExpected = Object.keys(expected).length
+    var actual = {}
+    q.on('data', function (pt) {
+      t.ok(expected[pt.value])
+      actual[pt.value] = true
+    })
+    q.on('end', function () {
+      t.equals(Object.keys(actual).length, numExpected)
+      t.end()
+    })
+  }
+})
